13d1e1d88aa0185212ea1371abfd70996d6a3f5e
kbhat1234 <karthikbhat@live.com>
2020-10-14 11:43:22 +0530

    add

--- variable.js
+++ variable.js
@@ -96,7 +96,13 @@ let contact = {
     }
 }
 
-console.log('contact address is ' +contact.address.city);
+console.log('contact address city is ' +contact.address.city);
+// contact address city is San Jose
+
 console.log('contact email is ' +contact.email);
+// contact email is john.doe@example.com
+
 console.log('contact details is \n');
 console.log(contact);
+// contact details is 
+//{firstName: "John", lastName: "Doe", email: "john.doe@example.com", phone: "(408)-555-9999", address: {…}}
